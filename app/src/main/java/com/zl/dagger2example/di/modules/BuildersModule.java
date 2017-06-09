bd7303a18b8d8b577056282b42e978b387b16bda
simplezhli <a05111993@163.com>
2017-06-09 21:02:25 +0800

    supplement

--- app/src/main/java/com/zl/dagger2example/di/modules/BuildersModule.java
+++ app/src/main/java/com/zl/dagger2example/di/modules/BuildersModule.java
@@ -3,7 +3,6 @@ package com.zl.dagger2example.di.modules;
 import android.app.Activity;
 
 import com.zl.dagger2example.di.components.MainComponent;
-import com.zl.dagger2example.di.components.UserComponent;
 import com.zl.dagger2example.di.scopes.ActivityScope;
 import com.zl.dagger2example.ui.ContributesActivity;
 import com.zl.dagger2example.ui.MainActivity;
@@ -19,18 +18,23 @@ import dagger.multibindings.IntoMap;
 /**
  * 作者：weilu on 2017/6/9 11:16
  */
-@Module(subcomponents = {MainComponent.class, UserComponent.class})
+@Module(subcomponents = {MainComponent.class})
 public abstract class BuildersModule {
 
     @Binds
     @IntoMap
     @ActivityKey(MainActivity.class)
     abstract AndroidInjector.Factory<? extends Activity> bindMainActivityInjectorFactory(MainComponent.Builder builder);
+//
+//    @Binds
+//    @IntoMap
+//    @ActivityKey(SecondActivity.class)
+//    abstract AndroidInjector.Factory<? extends Activity> bindSecondActivityInjectorFactory(UserComponent.Builder builder);
 
-    @Binds
-    @IntoMap
-    @ActivityKey(SecondActivity.class)
-    abstract AndroidInjector.Factory<? extends Activity> bindSecondActivityInjectorFactory(UserComponent.Builder builder);
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = UserModule.class)
+    abstract SecondActivity contributeSecondActivityInjector();
 
     @ActivityScope
     @ContributesAndroidInjector
